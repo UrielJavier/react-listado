b8374307bcfe507e3d6d32ba4f6d5ce16f027f07
Urieljavier <urieljavieraiz@gmail.com>
2020-01-20 12:53:52 +0100

    Updates

---
+++ precache-manifest.fb8bd0465021135b16cef96722076554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80debc9bee92183adcc0246a3553fad",
+    "url": "/react-lista/index.html"
+  },
+  {
+    "revision": "c121d69fa186197bed1a",
+    "url": "/react-lista/static/css/main.50edcbdf.chunk.css"
+  },
+  {
+    "revision": "d176a6cc879f296d27c8",
+    "url": "/react-lista/static/js/2.80b2daa8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-lista/static/js/2.80b2daa8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c121d69fa186197bed1a",
+    "url": "/react-lista/static/js/main.1f4419ba.chunk.js"
+  },
+  {
+    "revision": "bdf497abee6f92b86d3d",
+    "url": "/react-lista/static/js/runtime-main.b017199e.js"
+  }
+]);
